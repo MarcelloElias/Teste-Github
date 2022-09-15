61764eb1b6d6e0f245ecd7f380994dcf0bac4637
Marcello Elias <eelfelias123@gmail.com>
2022-09-14 22:15:05 -0300

    Criei uma aplicacao java de teste

---
+++ src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Teste Github");
+		String resposta = sc.next();
+		System.out.println("OK" + resposta);
+		
+		sc.close();
+	}
+
+}
